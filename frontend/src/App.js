62c87e52489643116dff22c0014769113d15960c
donghyeon.park <donghyeondev@gmail.com>
2024-01-08 17:55:53 +0900

    Add frontend structure and update gitignore
    This commit includes the addition of new frontend structure with React and styles with Tailwind CSS. The frontend includes new components and their styles.
    The .gitignore file is also updated to ignore the 'node_modules' and 'public' directories of the frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,124 @@
+import React, {useState, useEffect} from 'react';
+import './index.css';
+import axios from "axios";
+
+function App() {
+    const [characterName, setCharacterName] = useState("");
+    const [characterData, setCharacterData] =
+        useState({
+            worldName: "",
+            characterName: "",
+            characterClass: "",
+            characterClassLevel: "",
+            characterLevel: 0,
+            characterGuildName: "",
+            characterImage: "https://placehold.co/100x100",
+            combatPower: "",
+            characterPopularity: "",
+            stats:
+                [
+
+                ],
+            // ... Add other data as needed
+        });
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // This is where you would fetch data from the server when the component mounts
+    }, []);
+
+    const formatStatValue = (statValue) => {
+        //
+        return statValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
+    const fetchCharacterData = async () => {
+        if(characterName === "") {
+            console.log("Character Name is empty");
+            return;
+
+        }
+        setLoading(true);
+        axios.get("http://localhost:8080/character/overview?characterName="+ characterName)
+            .then((response) => {
+                console.log("Then Response :" + response);
+                const data = response.data;
+                characterData.characterLevel = data.characterBasic.characterLevel;
+                characterData.worldName = data.characterBasic.worldName;
+                characterData.characterClass = data.characterBasic.characterClass;
+                characterData.characterClassLevel = data.characterBasic.characterClassLevel;
+                characterData.characterGuildName = data.characterBasic.characterGuildName;
+                characterData.characterImage = data.characterBasic.characterImage;
+                characterData.combatPower = data.characterBasic.combatPower;
+                characterData.characterPopularity = data.characterBasic.characterPopularity;
+                characterData.stats = data.characterStat.finalStat;
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log("Error : " + error);
+                setLoading(false);
+            })
+        ;
+
+    }
+
+
+    return (
+        <div className="bg-[#31314e] min-h-screen flex justify-center items-center">
+            <div className="max-w-sm">
+                <div className="flex justify-center mt-4 space-x-2">
+                    <input
+                        type="text"
+                        value={characterName}
+                        onChange={(e) => setCharacterName(e.target.value)}
+                        placeholder="Character Name"
+                        className="py-2 px-4 rounded border-2 border-[#d77355] focus:outline-none focus:border-[#b56248]"
+                    />
+                    <button
+                        onClick={fetchCharacterData}
+                        className="bg-[#d77355] text-white py-2 px-4 rounded hover:bg-[#b56248]"
+                        disabled={loading}
+                    >
+                        {loading ? 'Loading...' : '조회'}
+                    </button>
+                </div>
+                <div className="bg-[#646964] p-5 rounded-t-lg shadow-lg">
+                    <div className="flex justify-between">
+                        <div>
+                            <p className="text-[#d77355] font-bold">CHARACTER INFO</p>
+                            <p className="text-white text-sm">Lv. {characterData.characterLevel}</p>
+
+                            <p className="text-white text-sm">{characterData.characterClass}</p>
+                            <p className="text-white text-sm">{characterData.worldName}</p>
+                        </div>
+                        <img src={characterData.characterImage} alt="https://placehold.co/250x250"
+                             className="square-full"/>
+                        <p className="text-white font-bold text-xl">{characterData.characterName}</p>
+                    </div>
+                    <div className="flex justify-around my-3">
+                    <button className="bg-[#d77355] text-white py-1 px-3 rounded">DETAIL</button>
+                        <button className="bg-[#647c64] text-white py-1 px-3 rounded">STAT</button>
+                    </div>
+                    <div className="bg-[#d6d4e0] p-2">
+                        <div className="bg-white p-3 rounded">
+                            {/*<p className="text-[#55415f] text-3xl font-bold">{characterData.combatPower}</p>*/}
+                            {characterData.stats.map((stat, index) => (
+                                <div key={`div-${index}`} className="flex justify-between my-2">
+                                    <span className="text-[#646964]" key={`name-${index}`}>{stat.statName}</span>
+                                    <span className="text-[#55415f]" key={`value-${index}`}>{stat.statValue}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-[#d6d4e0] p-5 rounded-b-lg shadow-lg">
+                    <div className="bg-white p-3 rounded">
+                        {/* Repeat for other character info sections */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
