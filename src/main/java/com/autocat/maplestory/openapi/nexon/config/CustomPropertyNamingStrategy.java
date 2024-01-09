a24e035c3bc61c9dd4a2ca59567b4893b71133ae
donghyeon.park <donghyeondev@gmail.com>
2024-01-09 17:19:06 +0900

    Refactor CustomPropertyNamingStrategy class
    Removed some overcomplicated code from the CustomPropertyNamingStrategy class and updated naming functions to optimize their operation. Specifically, a simplified approach is now implemented for naming setter methods by converting snake case to camel case names.

--- src/main/java/com/autocat/maplestory/openapi/nexon/config/CustomPropertyNamingStrategy.java
+++ src/main/java/com/autocat/maplestory/openapi/nexon/config/CustomPropertyNamingStrategy.java
@@ -2,62 +2,17 @@ package com.autocat.maplestory.openapi.nexon.config;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.cfg.MapperConfig;
-import com.fasterxml.jackson.databind.introspect.AnnotatedField;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
 
 public class CustomPropertyNamingStrategy extends PropertyNamingStrategies.SnakeCaseStrategy {
 
     @Override
     public String nameForSetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
-        return convertNameForSetter(defaultName);
+        return convertSnakeCaseToCamelCaseName(defaultName);
     }
 
-    @Override
-    public String nameForField(MapperConfig<?> config, AnnotatedField field, String defaultName) {
-        return convert(defaultName);
-    }
 
-    @Override
-    public String nameForGetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
-        return convert(defaultName);
-    }
-
-    // Custom method to handle names
-    private String convert(String input) {
-        StringBuilder output = new StringBuilder();
-        boolean upperCaseNextLetter = false;
-
-        // For each character in the input string
-        for (int i = 0; i < input.length(); i++) {
-            char currentChar = input.charAt(i);
-
-            // If the current character is an underscore
-            if (currentChar == '_') {
-                // Upper case the next valid letter
-                upperCaseNextLetter = true;
-            } else {
-                // If the current character is a digit
-                if (Character.isDigit(currentChar)) {
-                    // Add the digit to the output
-                    output.append(currentChar);
-                    // Do not upperCase the next letter
-                    upperCaseNextLetter = false;
-                } else {
-                    // If upper case is required, change the current char to upper case
-                    if (upperCaseNextLetter) {
-                        output.append(Character.toUpperCase(currentChar));
-                        upperCaseNextLetter = false;
-                    } else {
-                        output.append(currentChar);
-                    }
-                }
-            }
-        }
-
-        return output.toString();
-    }
-
-    private String convertNameForSetter(String input) {
+    private String convertSnakeCaseToCamelCaseName(String input) {
         StringBuilder stringBuilder = new StringBuilder();
 
         // We need to know if the previous character was upper case.
